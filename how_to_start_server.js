751bde45bf9945df8315ea198092332cc10f9563
Akash <akashjdongre@gmail.com>
2021-03-24 23:14:51 +0530

    Header response added

--- how_to_start_server.js
+++ how_to_start_server.js
@@ -1,10 +1,11 @@
 
 const http = require('http');
 const server = http.createServer((req,resp)=>
-{
+{	
+	resp.write(200 , {"Content-type" : "text/html"})
     resp.end('Hello World !');
 });
 
 server.listen(3000 , 'localhost',(err,data)=>{
     console.log('Server Started');
-})
+});
